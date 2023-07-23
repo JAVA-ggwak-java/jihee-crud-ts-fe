de4049f66aa8a042584e2b83f9ee2103427df03e
GHeeJeon <jhjeon_99@naver.com>
2023-07-23 16:27:42 +0900

    Deploy React app to GitHub Pages

--- server/app/page.js
+++ server/app/page.js
@@ -201,18 +201,18 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ 5621:
+/***/ 972:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1522, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 125, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6249, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 7844, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 8782, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1522, 23))
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 8782, 23))
 
 /***/ }),
 
-/***/ 3926:
+/***/ 2002:
 /***/ (() => {
 
 
